b18d7ea0b78b3cbf43edbd3ccdbdfdceeef40120
bandaru sai akhil jayanth <bandarusaiakhiljayanth@gmail.com>
2026-07-19 18:14:53 +0000

    Update requirements

--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,7 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
 nltk.download("punkt", quiet=True)
+nltk.download("punkt_tab", quiet=True)
 nltk.download("stopwords", quiet=True)
 nltk.download("wordnet", quiet=True)
 
